b63c835084a38420a4ffc988a5b3f7ac7df6a9a5
mayararaby <mayararaby246@gmail.com>
2021-05-26 12:21:51 -0700

    delete on click and add addeventlisnter instead of it

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-function showAddNewLink() {
-    let addNodes = document.getElementsByClassName('addSocial');
-    for (let i = 0; i < addNodes.length; i += 1) {
-        addNodes[i].style.display = 'block';
-    }
-
-}
+document.getElementById("addNewItem").addEventListener("click", addNewLink);
 
 function addNewLink() {
     let imageLink = document.getElementById("imageLink").value;
     let socialLink = document.getElementById("socialLink").value;
     document.getElementById("newSocialContainer").innerHTML += `<div class="flex">
-    <a href="https://github.com/" target="_blank">
-        <img class="newImage" src="https://www12.0zz0.com/2021/05/26/21/702597806.png" alt="">
+    <a href="${socialLink}" target="_blank">
+        <img class="newImage" src="${imageLink}" alt="">
 
     </a>
 </div>`
+    imageLink = ''
+    socialLink = ''
+
 }
